73b56effefaa91a47a432bc22e7c1a507251fea9
markphd <radical.mark@gmail.com>
2017-08-02 17:33:41 +0400

    implement search query sort

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,24 +1,64 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import escapeStringRegExp from 'escape-string-regexp'
+import sortBy from 'sort-by'
 
-function ListContacts(props) {
-	return (
-		<ol className="contact-list">
-			{ props.contacts.map((p) => (
-				<li key={p.id} className='contact-list-item'>
-					<div className="contact-avatar" style={{
-						backgroundImage: `url(${p.avatarURL})`
-					}}/>
-					<div className="contact-details">
-						<p>{p.name}</p>
-						<p>{p.email}</p>			
-					</div>
-					<button onClick={() => props.onDelete(p) } className='contact-remove'>
-						Remove
-					</button>
-				</li>
-			))}
-		</ol>
-	)
-}
+
+class ListContacts extends Component {
+	static propTypes = {
+	    contacts: PropTypes.array.isRequired,
+	    onDelete: PropTypes.func.isRequired
+	}
+
+	state = {
+		query: ''
+	}
+
+	updateQuery = (query) => {
+		this.setState({
+			query: query.trim()
+		})
+	}
+
+	render() {
+		const { contacts, onDelete } = this.props
+		const { query } = this.state
+
+		let showingContacts
+		const match = new RegExp(escapeStringRegExp(query), 'i')
+		showingContacts = query ? contacts.filter((contact) => match.test(contact.name)) : contacts
+		showingContacts.sort(sortBy('name'));
+
+		return(
+			<div className="list-contacts">
+				<div className="list-contacts-top">
+					<input
+						className="search-contacts"
+						type="text"
+						placeholder="Search"
+						value={query}
+						onChange={(event) => this.updateQuery(event.target.value)}
+					/>
+				</div>
+				<ol className="contact-list">
+					{ showingContacts.map((p) => (
+						<li key={p.id} className='contact-list-item'>
+							<div className="contact-avatar" style={{
+								backgroundImage: `url(${p.avatarURL})`
+							}}/>
+							<div className="contact-details">
+								<p>{p.name}</p>
+								<p>{p.email}</p>			
+							</div>
+							<button onClick={() => onDelete(p) } className='contact-remove'>
+								Remove
+							</button>
+						</li>
+					))}
+				</ol>
+			</div>
+		)
+	}
+} 
 
 export default ListContacts
