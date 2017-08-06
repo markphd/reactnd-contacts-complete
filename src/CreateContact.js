6bc482c238354fc4a4ea087f0b6daaccc70774fc
markphd <radical.mark@gmail.com>
2017-08-06 17:43:25 +0400

    serialize form data

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import ImageInput from './ImageInput'
 
+
 class CreateContact extends Component {
+	handleSubmit = (e) => {
+		e.preventDefault()
+	}
+
 	render() {
 		return(
-			<div>
+			<
+			div>
 				<Link to="/" className="close-create-contact"> Close </Link>
-				<form className="create-contact-form">
+				<form onSubmit={this.handleSubmit} className="create-contact-form">
 					<ImageInput
 						className="create-contact-avatar-input"
 						name="avatarURL"
 						maxHeight={64}
 					/>
 					<div className="create-contact-details">
-						<input type="text" name="Name" placeholder="Enter name" />
-						<input type="email" name="Email" placeholder="Enter email address" />
+						<input type="text" name="name" placeholder="Enter name" />
+						<input type="email" name="email" placeholder="Enter email address" />
 						<button>Add Button</button>
 					</div>
 				</form>
